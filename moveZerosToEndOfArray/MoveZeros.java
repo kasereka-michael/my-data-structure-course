f91893bff46b4548ae70746340ffe4c8a3a80a41
kasereka-michael <kaserekamichael23526@gmail.com>
2024-09-22 08:08:11 +0200

    commit of move zeros that is working perferctly

--- moveZerosToEndOfArray/MoveZeros.java
+++ moveZerosToEndOfArray/MoveZeros.java
@@ -3,22 +3,18 @@ package moveZerosToEndOfArray;
 public class MoveZeros {
     public void moveforZeros(int[] array) {
 
-        int index1 = 0, temp = 0, index2 = 0;
+        int index = 0;
 
         for (int i = 0; i < array.length; i++) {
-            if (array[index1] != 0 && array[index2] != 0) {
-                index2++;
-            } else if (array[index2] == 0) {
-                index1 = index2;
-                temp = array[index2];
-                index2++;
-                array[index1] = array[index2];
-                array[index2] = temp;
-            }
-            else {
-                index2++;
-            }
+            if(array[i] != 0 && array[index] == 0) {
 
+               int temp = array[i];
+                array[i] = array[index];
+                array[index] = temp;
+            }
+            if(array[index]!= 0){
+                index++;
+            }
         }
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
@@ -27,7 +23,7 @@ public class MoveZeros {
 
     public static void main(String[] args) {
         MoveZeros moveZeros = new MoveZeros();
-        int[] array = {2,4,6,0,2,0,1,8};
+        int[] array = {0,0,6,0,2,0,1,8};
         moveZeros.moveforZeros(array);
     }
 }
